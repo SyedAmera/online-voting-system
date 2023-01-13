c3a27158861ceda4c5af109347a45a2790c235cc
SyedAmera <118844451+SyedAmera@users.noreply.github.com>
2023-01-14 01:24:30 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>online voting system-dashboard</title>
+    </head>
+    <body>
+        <button>Back</button>
+        <button>Logout</button>
+        <h1>online voting system</h1>
+        <hr>
+        <div id="profile"></div>
+        <div id="group"></div>
+    </body>
+</html>
